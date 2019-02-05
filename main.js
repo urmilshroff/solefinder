0d68fbc82cc431884fa40ec51c5c59674471dcb6
urmilshroff <urmilshroff@gmail.com>
2019-02-05 19:03:26 +0530

    Commented out the script

--- main.js
+++ main.js
@@ -55,63 +55,63 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-var bar = document.getElementById('js-progressbar');
-
-UIkit.upload('.js-upload', {
-
-    url: '',
-    multiple: true,
-
-    beforeSend: function(environment) {
-        console.log('beforeSend', arguments);
-
-        // The environment object can still be modified here. 
-        // var {data, method, headers, xhr, responseType} = environment;
-
-    },
-    beforeAll: function() {
-        console.log('beforeAll', arguments);
-    },
-    load: function() {
-        console.log('load', arguments);
-    },
-    error: function() {
-        console.log('error', arguments);
-    },
-    complete: function() {
-        console.log('complete', arguments);
-    },
-
-    loadStart: function(e) {
-        console.log('loadStart', arguments);
-
-        bar.removeAttribute('hidden');
-        bar.max = e.total;
-        bar.value = e.loaded;
-    },
-
-    progress: function(e) {
-        console.log('progress', arguments);
-
-        bar.max = e.total;
-        bar.value = e.loaded;
-    },
-
-    loadEnd: function(e) {
-        console.log('loadEnd', arguments);
-
-        bar.max = e.total;
-        bar.value = e.loaded;
-    },
-
-    completeAll: function() {
-        console.log('completeAll', arguments);
-
-        setTimeout(function() {
-            bar.setAttribute('hidden', 'hidden');
-        }, 1000);
-
-        alert('Upload Completed');
-    }
-
-});
+// var bar = document.getElementById('js-progressbar');
+// 
+// UIkit.upload('.js-upload', {
+// 
+//     url: '',
+//     multiple: true,
+// 
+//     beforeSend: function(environment) {
+//         console.log('beforeSend', arguments);
+// 
+//         // The environment object can still be modified here. 
+//         // var {data, method, headers, xhr, responseType} = environment;
+// 
+//     },
+//     beforeAll: function() {
+//         console.log('beforeAll', arguments);
+//     },
+//     load: function() {
+//         console.log('load', arguments);
+//     },
+//     error: function() {
+//         console.log('error', arguments);
+//     },
+//     complete: function() {
+//         console.log('complete', arguments);
+//     },
+// 
+//     loadStart: function(e) {
+//         console.log('loadStart', arguments);
+// 
+//         bar.removeAttribute('hidden');
+//         bar.max = e.total;
+//         bar.value = e.loaded;
+//     },
+// 
+//     progress: function(e) {
+//         console.log('progress', arguments);
+// 
+//         bar.max = e.total;
+//         bar.value = e.loaded;
+//     },
+// 
+//     loadEnd: function(e) {
+//         console.log('loadEnd', arguments);
+// 
+//         bar.max = e.total;
+//         bar.value = e.loaded;
+//     },
+// 
+//     completeAll: function() {
+//         console.log('completeAll', arguments);
+// 
+//         setTimeout(function() {
+//             bar.setAttribute('hidden', 'hidden');
+//         }, 1000);
+// 
+//         alert('Upload Completed');
+//     }
+// 
+// });
