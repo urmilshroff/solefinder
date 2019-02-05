256aad18c18bf0e590228f660e207ac22e63ff0c
urmilshroff <urmilshroff@gmail.com>
2019-02-05 16:42:04 +0530

    Moved internal JS into external file

--- main.js
+++ main.js
@@ -48,3 +48,117 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+var bar = document.getElementById('js-upload');
+
+UIkit.upload('.js-upload', {
+
+url: '',
+multiple: true,
+
+beforeSend: function () {
+    console.log('beforeSend', arguments);
+},
+beforeAll: function () {
+    console.log('beforeAll', arguments);
+},
+load: function () {
+    console.log('load', arguments);
+},
+error: function () {
+    console.log('error', arguments);
+},
+complete: function () {
+    console.log('complete', arguments);
+},
+
+loadStart: function (e) {
+    console.log('loadStart', arguments);
+
+    bar.removeAttribute('hidden');
+    bar.max = e.total;
+    bar.value = e.loaded;
+},
+
+progress: function (e) {
+    console.log('progress', arguments);
+
+    bar.max = e.total;
+    bar.value = e.loaded;
+},
+
+loadEnd: function (e) {
+    console.log('loadEnd', arguments);
+
+    bar.max = e.total;
+    bar.value = e.loaded;
+},
+
+completeAll: function () {
+    console.log('completeAll', arguments);
+
+    setTimeout(function () {
+        bar.setAttribute('hidden', 'hidden');
+    }, 1000);
+
+    alert('Upload Completed');
+}
+
+});
+
+var bar = document.getElementById('js-progressbar');
+
+UIkit.upload('.js-upload', {
+
+    url: '',
+    multiple: true,
+
+    beforeSend: function () {
+        console.log('beforeSend', arguments);
+    },
+    beforeAll: function () {
+        console.log('beforeAll', arguments);
+    },
+    load: function () {
+        console.log('load', arguments);
+    },
+    error: function () {
+        console.log('error', arguments);
+    },
+    complete: function () {
+        console.log('complete', arguments);
+    },
+
+    loadStart: function (e) {
+        console.log('loadStart', arguments);
+
+        bar.removeAttribute('hidden');
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    progress: function (e) {
+        console.log('progress', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    loadEnd: function (e) {
+        console.log('loadEnd', arguments);
+
+        bar.max = e.total;
+        bar.value = e.loaded;
+    },
+
+    completeAll: function () {
+        console.log('completeAll', arguments);
+
+        setTimeout(function () {
+            bar.setAttribute('hidden', 'hidden');
+        }, 1000);
+
+        alert('Upload Completed');
+    }
+
+});
