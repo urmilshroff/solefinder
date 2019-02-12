5ebbda9119ad127e3bd8cb43fd60a05ab6ea736d
urmilshroff <urmilshroff@gmail.com>
2019-02-12 20:16:34 +0530

    Updated

--- scripts/solefinder.js
+++ scripts/solefinder.js
@@ -1,21 +1,20 @@
 var remote = require('electron').remote;
 var fs = remote.require('fs');
+// var element = document.getElementById("impath");
 
-
-remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
-        filters: [{
-            name: 'Images',
-            extensions: ['jpg']
-        }]
-    },
-    function(filepaths, bookmarks) {
-        //read image (note: use async in production)
-        var _img = fs.readFileSync(filepaths[0]).toString('base64');
-        //example for .png
-        var _out = '<img src="data:image/png;base64,' + _img + '" />';
-        //render/display
-        var _target = document.getElementById('image_container');
-        _target.insertAdjacentHTML('beforeend', _out);
-        console.log(filepaths[0].toString('base64'));
-        return;
-    });
+function imagePicker() {
+    remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+            filters: [{
+                name: 'Images',
+                extensions: ['jpg', 'jpeg', 'png']
+            }]
+        },
+        function(filepaths, bookmarks) {
+            var path = filepaths[0].toString('base64') //contains the filepath of the image
+            var _img = fs.readFileSync(filepaths[0]).toString('base64');
+            var _out = '<img src="data:image/png;base64,' + _img + '" />';
+            console.log(path);
+            // element.innerHTML=path;
+            return path;
+        });
+}
