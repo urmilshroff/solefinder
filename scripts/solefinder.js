05e9130a214ea420066bc08c326b99cfcffce09c
urmilshroff <urmilshroff@gmail.com>
2019-02-15 17:57:57 +0530

    Updated

--- scripts/solefinder.js
+++ scripts/solefinder.js
@@ -1,6 +1,7 @@
-var remote = require('electron').remote;
-var fs = remote.require('fs');
-// var element = document.getElementById("impath");
+var remote = require('electron').remote
+var fs = remote.require('fs')
+var python = require("python-shell")
+var path = require("path")
 
 function imagePicker() {
     remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
@@ -11,10 +12,24 @@ function imagePicker() {
         },
         function(filepaths, bookmarks) {
             var path = filepaths[0].toString('base64') //contains the filepath of the image
-            var _img = fs.readFileSync(filepaths[0]).toString('base64');
-            var _out = '<img src="data:image/png;base64,' + _img + '" />';
-            console.log(path);
-            // element.innerHTML=path;
-            return path;
-        });
+            var _img = fs.readFileSync(filepaths[0]).toString('base64')
+            var _out = '<img src="data:image/png;base64,' + _img + '" />'
+            console.log(path)
+            // callingPython(path)
+            return path
+        })
+}
+
+function callingPython() {
+    var options = {
+        pythonPath: '/usr/local/bin/python3', //for Python 3 on Linux/macOS
+        scriptPath: path.join(__dirname, '/backend/'),
+        // args : []
+    }
+
+    var callPython = new python('test.py', options)
+
+    callPython.on('message', function(message) {
+        window.alert(message)
+    })
 }
