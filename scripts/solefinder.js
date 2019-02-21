fb5141a121792d39d313d3c80ca33932e3206dcc
prince1998 <priyanshramnani21@gmail.com>
2019-02-22 00:50:58 +0530

    Detects the user input image
    Detects the user input image and changed the position of button

--- scripts/solefinder.js
+++ scripts/solefinder.js
@@ -2,6 +2,7 @@ var remote = require('electron').remote
 var fs = remote.require('fs')
 var python = require("python-shell")
 var path = require("path")
+var pathToImage
 
 function imagePicker() {
     remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
@@ -11,7 +12,7 @@ function imagePicker() {
             }]
         },
         function(filepaths, bookmarks) {
-            var pathToImage = filepaths[0].toString('base64') //contains the filepath of the image
+            pathToImage = filepaths[0].toString('base64') //contains the filepath of the image
             var _img = fs.readFileSync(filepaths[0]).toString('base64')
             var _out = '<img src="data:image/png;base64,' + _img + '" />'
             console.log(pathToImage)
@@ -24,7 +25,7 @@ function callingPython() {
     var options = {
         pythonPath: '/usr/local/bin/python3', //for Python 3 on Linux/macOS
         scriptPath: path.join(__dirname, '/backend/'),
-        args: ['--graph=tf_files/retrained_graph.pb', '--labels=tf_files/retrained_labels.txt', '--output_layer=final_result', '--input_height=299', '--input_width=299', '--image=test_images/adidas_zeta_test3.jpg'] //change the test image filename
+        args: ['--graph=tf_files/retrained_graph.pb', '--labels=tf_files/retrained_labels.txt', '--output_layer=final_result', '--input_height=299', '--input_width=299', '--image='+pathToImage] //change the test image filename
     }
 
     var callPython = new python('label_image.py', options)
