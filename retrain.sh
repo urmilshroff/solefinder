3fca590614c194fc15146ca6b7361b3c7bd36c96
urmilshroff <urmilshroff@gmail.com>
2019-03-05 15:53:59 +0530

    Revert "Retrained entire model"
    This reverts commit dec5a84a027aff4f20140b29c3cfda71b7b87625.

--- retrain.sh
+++ retrain.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Removing old tf_files folder..."
-rm -r tf_files
+echo -e "Retraining model..."
 
-read -p "Enter number of training steps: " steps
-echo -e "Retraining model with $steps steps..."
-
-python3 backend/retrain.py --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=$steps --model_dir=inception --summaries_dir=tf_files/training_summaries --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --image_dir=datasets
+python3 backend/retrain.py --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=500 --model_dir=inception --summaries_dir=tf_files/training_summaries --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --image_dir=/datasets
 
 echo -e "Done."
