253782252bd3e8576cb02bd5dffc19f049ff2031
urmilshroff <urmilshroff@gmail.com>
2019-03-12 15:07:23 +0530

    Updated

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ read -p "Which test image do you want to use? " choice
 
 echo -e "\nRunning SoleFinder to detect $choice..."
 
-# python3 scripts/label_image.py --graph=tf_files/retrained_graph.pb --labels=tf_files/retrained_labels.txt --output_layer=final_result --input_height=299 --input_width=299 --image=test_images/$choice
-
 shoe=$(python3 backend/label_image.py --graph=tf_files/retrained_graph.pb --labels=tf_files/retrained_labels.txt --output_layer=final_result --input_height=299 --input_width=299 --image=test_images/$choice | tail -1)
 
 echo -e "\nDetected shoe was $shoe!"
